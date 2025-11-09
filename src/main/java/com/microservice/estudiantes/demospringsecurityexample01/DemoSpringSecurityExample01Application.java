4dcee301f12dca481867fd9093a2e5f1e985e9ad
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-11-09 08:58:35 -0500

    init

---
+++ src/main/java/com/microservice/estudiantes/demospringsecurityexample01/DemoSpringSecurityExample01Application.java
@@ -0,0 +1,13 @@
+package com.microservice.estudiantes.demospringsecurityexample01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringSecurityExample01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringSecurityExample01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/microservice/estudiantes/demospringsecurityexample01/DemoSpringSecurityExample01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.estudiantes.demospringsecurityexample01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringSecurityExample01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
